b524aa8684cd28edc98aec49baa6249098f7cecb
Divya Shrestha <divya@Divyas-MacBook-Air.local>
2024-04-02 22:18:28 -0400

    Implemented writing to and reading from a large NumPy array for memory benchmark

--- memory_benchamrk.py
+++ memory_benchamrk.py
@@ -12,3 +12,15 @@ def main():
     my_arr = np.zeros(500000000)
     dummy_read_value = 0
     total_time = 0
+    
+    start = time.time()
+    # Write to 5 × 10^9 different array elements, 4 bytes each time
+    for i in range(len(my_arr)):
+        my_arr[i] = i
+
+    # Read from 5 × 10^9 different array elements, 4 bytes each time
+    for i in range(len(my_arr)):
+        dummy_read_value = my_arr[i]
+
+    end = time.time()
+    total_time = end - start
